77bd4fe1c3693008dda41daabf42a0ceedde8b1e
nmcneany <nmcneany@gmail.com>
2025-09-14 16:51:09 -0500

    engine working

--- packages/rails/turboflow-rails/lib/turboflow-rails.rb
+++ packages/rails/turboflow-rails/lib/turboflow-rails.rb
@@ -4,3 +4,15 @@ require "turboflow/version"
 require "turboflow/configuration"
 require "turboflow/engine"
 require "turboflow/helpers"
+
+module TurboFlow
+  class << self
+    def configuration
+      @configuration ||= Configuration.new
+    end
+
+    def configure
+      yield(configuration)
+    end
+  end
+end
